00f225f2a281ec1ac278b89db6b5e9f7b6c44cfe
Artyom Titov <artdesirer@gmail.com>
2020-08-15 03:39:17 -0600

    the better & more proper way of doing it. + audit fix

--- angular-image-search/src/app/image-search/image-search.component.ts
+++ angular-image-search/src/app/image-search/image-search.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ImageApiService, APIResult, ImageResult } from '../image-api.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-image-search',
@@ -9,18 +10,16 @@ import { ImageApiService, APIResult, ImageResult } from '../image-api.service';
 export class ImageSearchComponent implements OnInit {
   searchTerm = '';
   loading = false;
-  images: string[] = [];
+  images$: Observable<string[]>;
 
   constructor(private imageAPIService: ImageApiService) { }
 
   ngOnInit(): void {}
 
   formSubmitted() {
-    this.images = [];
     this.loading = true;
-    this.imageAPIService.getImages(this.searchTerm)
-      .subscribe((images: string[]) => {
-        this.images = images;
+    this.images$ = this.imageAPIService.getImages(this.searchTerm);
+    this.images$.subscribe((images: string[]) => {
         this.loading = false;   
       });
   }
